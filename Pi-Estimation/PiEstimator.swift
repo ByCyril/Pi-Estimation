72f06c4c5ac8ef39d555837758cb602802431f48
Cyril Garcia <garciacyrilivar5768@yahoo.com>
2021-03-15 08:03:57 -0700

    init

---
+++ Pi-Estimation/PiEstimator.swift
@@ -0,0 +1,48 @@
+//
+//  PiEstimator.swift
+//  Pi-Estimation
+//
+//  Created by Cyril Garcia on 3/15/21.
+//
+
+import Foundation
+
+infix operator **
+
+func **(lhs: Float, rhs: Float) -> Float {
+    return pow(lhs, rhs)
+}
+
+final class PiEstimator {
+    
+    func isCoPrime(_ a: Int,_ b: Int) -> Bool {
+        return 1 == isCoPrimeHelper(a, b, a / b, a % b)
+    }
+    
+    func isCoPrimeHelper(_ a: Int,_ b: Int,_ q: Int,_ r: Int) -> Int {
+        if r == 0 {
+            return b
+        }
+        return isCoPrimeHelper(b, r, b / r, b % r)
+    }
+    
+    func simulation(_ trials: Int = 1000) {
+        
+        var x: Float = 0 // number of coprimes found
+
+        for _ in 0..<trials {
+            let a = Int.random(in: 1...100_000)
+            let b = Int.random(in: 1...100_000)
+            
+            if a > b {
+                x += (isCoPrime(a,b)) ? 1 : 0
+            } else {
+                x += (isCoPrime(b,a)) ? 1 : 0
+            }
+        }
+
+        let pi = (6.0 / (x / Float(trials)))**0.5
+        print("Estimate of Pi is:",pi)
+    }
+    
+}
---
+++ Pi-Estimation/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Pi-Estimation
+//
+//  Created by Cyril Garcia on 3/15/21.
+//
+
+import Foundation
+
+PiEstimator().simulation()
+PiEstimator().simulation()
+PiEstimator().simulation()
+PiEstimator().simulation()
+PiEstimator().simulation(5000)
+PiEstimator().simulation(50000)
